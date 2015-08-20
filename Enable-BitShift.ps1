27cfd3327d6bbdc572e0eccb5afcc8e9958cad50
coolmacool <rshacklef0rd@outlook.com>
2015-08-20 14:12:02 -0500

    cleanup
    shortened line lengths as much as possible, added regions for easier
    reading

--- Enable-BitShift.ps1
+++ Enable-BitShift.ps1
@@ -3,27 +3,44 @@
     # Big thanks to Matt Graeber for showing how it is possible to assembly .NET methods with CIL opcodes
     # and for the overall fueling of the fire that is my PowerShell obsession
 
+    #region Create TypeBuilder
     $Domain          = [AppDomain]::CurrentDomain
     $DynAssembly     = New-Object System.Reflection.AssemblyName -ArgumentList @('PoShBitwiseAssembly')
-    $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, 
+                                                     [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
     $ModuleBuilder   = $AssemblyBuilder.DefineDynamicModule('PoShBitwiseModule', $False)
-    $TypeBuilder     = $ModuleBuilder.DefineType('PoShBitwiseBuilder', [System.Reflection.TypeAttributes]'Public, Sealed, AnsiClass, AutoClass',[Object])
+    $TypeBuilder     = $ModuleBuilder.DefineType('PoShBitwiseBuilder', 
+                                                 [System.Reflection.TypeAttributes]'Public, Sealed, AnsiClass, AutoClass',
+                                                 [Object])
+    #endregion
 
-    $MethodBuilder   = $TypeBuilder.DefineMethod('Lsh', [System.Reflection.MethodAttributes]'Public,Static,HideBySig,NewSlot', [Int32], [type[]]([Int32],[UInt32]))
-    $MethodBuilder.SetImplementationFlags([System.Reflection.MethodImplAttributes]'Managed')
+    #region Define Lsh Method
+    $MethodBuilder   = $TypeBuilder.DefineMethod('Lsh', 
+                                                 [System.Reflection.MethodAttributes]'Public,Static,HideBySig,NewSlot', 
+                                                 [Int32], 
+                                                 [type[]]([Int32],[UInt32]))
+    $MethodBuilder.SetImplementationFlags([System.Reflection.MethodImplAttributes]::IL)
     $ILGen = $MethodBuilder.GetILGenerator(2)
     $ILGen.Emit([Reflection.Emit.OpCodes]::Ldarg_0)
     $ILGen.Emit([Reflection.Emit.OpCodes]::Ldarg_1)
     $ILGen.Emit([Reflection.Emit.OpCodes]::Shl)
     $ILGen.Emit([Reflection.Emit.OpCodes]::Ret)
+    #endregion
 
-    $MethodBuilder   = $TypeBuilder.DefineMethod('Rsh', [System.Reflection.MethodAttributes]'Public,Static,HideBySig,NewSlot', [Int32], [type[]]([Int32],[UInt32]))
-    $MethodBuilder.SetImplementationFlags([System.Reflection.MethodImplAttributes]'Managed')
+    #region Define Rsh Method
+    $MethodBuilder   = $TypeBuilder.DefineMethod('Rsh', 
+                                                 [System.Reflection.MethodAttributes]'Public,Static,HideBySig,NewSlot', 
+                                                 [Int32], 
+                                                 [type[]]([Int32],[UInt32]))
+    $MethodBuilder.SetImplementationFlags([System.Reflection.MethodImplAttributes]::IL)
     $ILGen = $MethodBuilder.GetILGenerator(2)
     $ILGen.Emit([Reflection.Emit.OpCodes]::Ldarg_0)
     $ILGen.Emit([Reflection.Emit.OpCodes]::Ldarg_1)
     $ILGen.Emit([Reflection.Emit.OpCodes]::Shr)
     $ILGen.Emit([Reflection.Emit.OpCodes]::Ret)
+    #endregion
 
+    # Generate RuntimeType and assign to global variable
     $Global:Bitwise = $TypeBuilder.CreateType()
 }
+
